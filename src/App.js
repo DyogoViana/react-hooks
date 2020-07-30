eecdcf882fac409bf87e3a7d44527c7d9a371b10
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-07-30 16:25:07 -0300

    0ook useState.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-	return <h1>Follow the white rabb1t</h1>;
+	const [tech, setTech] = useState(['ReactJS', 'React Native']);
+	const [newTech, setNewTech] = useState('');
+
+	function handleAdd() {
+		setTech([...tech, newTech]);
+		setNewTech(''); // Limpa o campo input.
+	}
+
+	return (
+		<>
+			<ul>
+				{tech.map((techs) => (
+					<li key={techs}> {techs} </li>
+				))}
+			</ul>
+
+			<input
+				value={newTech}
+				onChange={(event) => setNewTech(event.target.value)}
+			/>
+			<button type="button" onClick={handleAdd}>
+				Adicionar nova tecnológia
+			</button>
+		</>
+	);
 }
 
 export default App;
