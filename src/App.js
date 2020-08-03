ad246319cd380b8c1a0dff28457989fd1ddc6083
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-08-03 16:25:18 -0300

    Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-	const [tech, setTech] = useState(['ReactJS', 'React Native']);
+	const [tech, setTech] = useState([]);
 	const [newTech, setNewTech] = useState('');
 
 	function handleAdd() {
 		setTech([...tech, newTech]);
 		setNewTech(''); // Limpa o campo input.
 	}
+
+	useEffect(() => {
+		// Verifixa se tem algo no localStorage.
+		const storageTech = localStorage.getItem('tech');
+
+		// Caso tenha algo no localStorage, executa o 'setTech'.
+		if (storageTech) {
+			setTech(JSON.parse(storageTech));
+		}
+	}, []);
+
+	// Update
+	useEffect(() => {
+		localStorage.setItem('tech', JSON.stringify(tech));
+	}, [tech]); // executa toda vez que o valor 'tech' for auterado.
 
 	return (
 		<>
